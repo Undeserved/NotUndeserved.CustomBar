9927daad8968c5eda2946ac6fe0e1248e23c519a
Undeserved <acostaarrietac@gmail.com>
2025-05-02 17:07:48 -0500

    Visual update, settings token refactor
    Made the UI less visually grating
    Changed the exportable settings dto to include all values that involve a certain degree of persistence
    Fixed the import token and save settings methods to automatically propagate changes to all open tabs

--- src/NotUndeserved.Twitch.CustomProgBar/Application/Dtos/OAuthTokenDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NotUndeserved.Twitch.CustomProgBar.Application.Dtos {
-    public class OAuthTokenDto {
-        public string AccessToken { get; set; } = "";
-        public string RefreshToken { get; set; } = "";
-        public string ExpiresAt { get; set; } = "";
-        public string ChannelName { get; set; } = "";
-    }
-}
---
+++ src/NotUndeserved.Twitch.CustomProgBar/Application/Dtos/SettingsDto.cs
@@ -0,0 +1,14 @@
+﻿namespace NotUndeserved.Twitch.CustomProgBar.Application.Dtos {
+    public class SettingsDto {
+        public string AccessToken { get; set; } = "";
+        public string RefreshToken { get; set; } = "";
+        public string ExpiresAt { get; set; } = "";
+        public string ChannelName { get; set; } = "";
+        public string CustomBarUrl { get; set; } = "";
+        public string CustomDialUrl { get; set; } = "";
+        public string BarSegments { get; set; } = "";
+        public string RedemptionIncrease { get; set; } = "";
+        public string RedemptionDecrease { get; set; } = "";
+        public string RedemptionReset { get; set; } = "";
+    }
+}
